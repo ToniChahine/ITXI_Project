968251898fc93648c52aa3053950e89d53560c3c
ToniChahine <141623096+ToniChahine@users.noreply.github.com>
2023-08-09 19:27:57 +0300

    final result

--- spotifyapp/src/component/searchBar.jsx
+++ spotifyapp/src/component/searchBar.jsx
@@ -1,4 +1,4 @@
-import React ,{useState}from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default function SearchBar({link,flag,searchArtist, searchKey,setSearchKeyy,artistFound,artistName,setArtistNamee}) {
